54f9f9c9c9e16af9aba1a23bf1deb9860e8432e6
linuk <hello.linuk@gmail.com>
2018-01-26 19:21:34 +0000

    first iOS app commit

--- src/app/README.md
+++
@@ -1,3 +0,0 @@
-# The iLost Mobile Application 
-
-This directory should contain both front-end and back-end sections.
---
+++ src/ios-app/iLost/iLost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iLost
+//
+//  Created by Nyo on 26/01/2018.
+//  Copyright © 2018 GSoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
