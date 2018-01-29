4246b721b864114fde7efd32b0035a1e43949ed6
linuk <hello.linuk@gmail.com>
2018-01-29 00:59:15 +0000

    Updated: added Item Model
    Added the item model and unit tests.
    
    Updated #83

--- src/ios-app/iLost/iLostTests/iLostTests.swift
+++ src/ios-app/iLost/iLostTests/iLostTests.swift
@@ -32,5 +32,25 @@ class iLostTests: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
+
     
+    // MARK: Item Class Test
+    
+    // Confirm that the Item initialiser returns a Item object when passed valid parameters
+    func testItemInitialiserSucceeds() {
+        let validItem = Item.init(name: "Valid Item", photo: nil, trackerID: "fake ID")
+        XCTAssertNotNil(validItem)
+    }
+    
+    // Confirm that the Item initialiser return nil when passed a empty namd or empty trackID
+    func testItemInitialiserFailes() {
+        
+        // Empty name
+        let emptyNameItem = Item.init(name: "", photo: nil, trackerID: "fake ID")
+        XCTAssertNil(emptyNameItem)
+        
+        // Empty tracker ID
+        let emptyTrackerID = Item.init(name: "Item Name", photo: nil, trackerID: "")
+        XCTAssertNil(emptyTrackerID)
+    }
 }
